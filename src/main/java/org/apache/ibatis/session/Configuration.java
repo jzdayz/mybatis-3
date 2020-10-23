700162c78c7d26cba4f03461772100dc65f24587
huqingfeng <1397649957@qq.com>
2020-10-23 18:31:09 +0800

    阅读代码

--- src/main/java/org/apache/ibatis/session/Configuration.java
+++ src/main/java/org/apache/ibatis/session/Configuration.java
@@ -99,7 +99,7 @@ import org.apache.ibatis.type.TypeHandlerRegistry;
  * @author Clinton Begin
  */
 public class Configuration {
-
+  // 环境
   protected Environment environment;
 
   protected boolean safeRowBoundsEnabled;
